26f7f531f996be37546edc22a3a7ea66a765d3ee
Emmanuel ZIDEL-CAUFFET <emmanuel.zidel@gmail.com>
2021-08-23 22:06:18 +0200

    Use a list of secret.
    Signed-off-by: Emmanuel ZIDEL-CAUFFET <emmanuel.zidel@gmail.com>

--- target/tf/secret.tf
+++ target/tf/secret.tf
@@ -6,8 +6,8 @@ resource "google_secret_manager_secret" "jwt_signing_key" {
 }
 
 resource "google_secret_manager_secret" "channel_tokens" {
-  for_each = var.channel_token_list
-  secret_id = "discord_${each.value.channel}"
+  for_each = {for channel_token in var.channel_token_list: channel_token.channel => channel_token}
+  secret_id = "discord_token_${each.value.channel}"
   replication {
     automatic = true
   }
@@ -18,23 +18,9 @@ resource "google_secret_manager_secret_version" "jwt_signing_key" {
   secret_data = var.jwt_signing_key
 }
 
-resource "google_secret_manager_secret_version" "channel_tokens" {
-  for_each    = var.channel_token_list
-  secret      = google_secret_manager_secret.discord_${each.value.channel}.id
-  secret_data = each.value.token
-}
-
 resource "google_secret_manager_secret_iam_member" "jwt_signing_key" {
   project   = data.google_project.current_project.project_id
   secret_id = google_secret_manager_secret.jwt_signing_key.secret_id
-  role      = "roles/secretmanager.secretAccessor"
-  member    = "serviceAccount:${google_service_account.sendmsg-svc.email}"
-}
-
-resource "google_secret_manager_secret_iam_member" "channel_tokens" {
-  for_each  = var.channel_token_list
-  project   = data.google_project.current_project.project_id
-  secret_id = google_secret_manager_secret.discord_${each.value.channel}.secret_id
   role      = "roles/secretmanager.secretAccessor"
   member    = "serviceAccount:${google_service_account.sendmsg-svc.email}"
 }
