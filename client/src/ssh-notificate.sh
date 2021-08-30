15cc55f72c01e384f086b85a8a8c517c1a496ae9
Emmanuel ZIDEL-CAUFFET <emmanuel.zidel@gmail.com>
2021-08-30 21:01:58 +0200

    Add 3 attempts to call the distant API to notificate. Change the messages Do not source the file since source is not allowed when the shell starts with SSH Hide the dig errors
    Signed-off-by: Emmanuel ZIDEL-CAUFFET <emmanuel.zidel@gmail.com>

--- client/src/ssh-notificate.sh
+++ client/src/ssh-notificate.sh
@@ -3,9 +3,9 @@
 # Define the time when the script is called and so when the user connects
 CONNECT_TIME=$(date '+%Y-%m-%d %H:%M:%S')
 
-# Define the environment file path
-DIRNAME=$(dirname "$(dirname "$(readlink -f "$0")")")
-ENV_FILE="$DIRNAME/webhook.env"
+# Define the parameter file path
+DIRNAME=$(dirname "$(readlink -f "$0")")
+PRM_FILE="$DIRNAME/webhook.prm"
 
 # Define the useful parameters
 HOSTNAME=$(hostname)
@@ -15,28 +15,46 @@ PORT_SRC=$(echo "$SSH_CONNECTION" | awk -F' ' '{print $2}')
 IP_DST=$(echo "$SSH_CONNECTION"   | awk -F' ' '{print $3}')
 PORT_DST=$(echo "$SSH_CONNECTION" | awk -F' ' '{print $4}')
 
-DNS_SRC_REVERSE=$(dig -x "$IP_SRC" +short)
+DNS_SRC_REVERSE=$(dig -x "$IP_SRC" +short 2>/dev/null)
 if [ "$DNS_SRC_REVERSE" != "" ]
 then
 	SRC_MSG=" ($DNS_SRC_REVERSE)"
 fi
 
 # Log the connection
-MESSAGE="$USER($UID) has just logged in from $IP_SRC$SRC_MSG with port $PORT_SRC to $IP_DST ($HOSTNAME) with port $PORT_DST at $CONNECT_TIME"
+MESSAGE="User '$USER' has just logged in from $IP_SRC$SRC_MSG with port $PORT_SRC to $IP_DST ($HOSTNAME) with port $PORT_DST at $CONNECT_TIME"
 echo "[i] $MESSAGE"
-logger -t ssh-wrapper "$MESSAGE"
+logger -t ssh-wrapper "[i] $MESSAGE"
 
 # Push the notification through the webhook
-if [ -f "$ENV_FILE" ]
+if [ -f "$PRM_FILE" ]
 then
-	source "$ENV_FILE"
-	curl -X POST -H "Content-Type: application/json"  -d "{\"username\":\"$HOSTNAME\", \"content\":\"$MESSAGE\"}" "$SSHCONNECT_WEBHOOK_URL"
-	RSLT="$?"
-	if [ "$RSLT" != "0" ]
+	SSHCONNECT_WEBHOOK_URL=$(grep "^https://" "$PRM_FILE" | tail -n1)
+	if [ "$SSHCONNECT_WEBHOOK_URL" = "" ]
 	then
-		logger -t ssh-wrapper "[-] Error in the curl command to the webhook to notify an SSH connection [error code ($RSLT)]"
+		logger -t ssh-wrapper "[-] Error no webhook URL defined"
+	else
+		CURL_COUNT=0
+		while [ "$CURL_COUNT" -le 3 ]
+		do
+			CURL_COUNT=$(( CURL_COUNT+1 ))
+			curl -X POST -H "Content-Type: application/json"  -d "{\"content\":\"$MESSAGE\"}" "$SSHCONNECT_WEBHOOK_URL" 1>/dev/null 2>&1
+			RSLT="$?"
+			if [ "$RSLT" -eq 0 ]
+			then
+				break
+			fi
+		done
+
+		if [ "$RSLT" -ne 0 ]
+		then
+			logger -t ssh-wrapper "[-] Error in the curl command to the webhook to notify an SSH connection [error code ($RSLT)]"
+		else
+			logger -t ssh-wrapper "[i] Notification sent after $CURL_COUNT attempt(s)"
+		fi
 	fi
 else
-	logger -t ssh-wrapper "[-] Error no environment file found to notify an SSH connection"
+	echo "[-] No parameter file found"
+	logger -t ssh-wrapper "[-] Error no parameter file found to notify an SSH connection"
 fi
 
